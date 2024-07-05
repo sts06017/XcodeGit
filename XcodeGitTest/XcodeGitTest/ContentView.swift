a48f24f7dce52253814626719bc20ade82ab375f
kim dong kyung <kdkd422@naver.com>
2024-07-05 18:08:03 +0900

    first

---
+++ XcodeGitTest/XcodeGitTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeGitTest
+//
+//  Created by 김동경 on 2024/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeGitTest/XcodeGitTest/XcodeGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitTestApp.swift
+//  XcodeGitTest
+//
+//  Created by 김동경 on 2024/07/05.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
